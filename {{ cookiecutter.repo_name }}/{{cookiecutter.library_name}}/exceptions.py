edf76bd583f391f3b15a53775b66092f2f575870
John Purcell <john.purcell@crowdstrike.com>
2018-07-09 19:17:03 -0700

    fixing title casing

--- {{ cookiecutter.repo_name }}/{{cookiecutter.library_name}}/exceptions.py
+++ {{ cookiecutter.repo_name }}/{{cookiecutter.library_name}}/exceptions.py
@@ -1,3 +1,3 @@
 """custom exceptions for {{cookiecutter.library_name}}"""
-class {{title(cookiecutter.library_name.replace('_', ''))}}(Exception):
+class {{cookiecutter.library_name.replace('_', '').title()}}(Exception):
     """base exception class for project"""
