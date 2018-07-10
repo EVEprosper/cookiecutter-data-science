dde96f80703c3751e6448bbabf71d2d89729259b
John Purcell <john.purcell@crowdstrike.com>
2018-07-09 19:17:44 -0700

    adding Exception to base exception name

--- {{ cookiecutter.repo_name }}/{{cookiecutter.library_name}}/exceptions.py
+++ {{ cookiecutter.repo_name }}/{{cookiecutter.library_name}}/exceptions.py
@@ -1,3 +1,3 @@
 """custom exceptions for {{cookiecutter.library_name}}"""
-class {{cookiecutter.library_name.replace('_', '').title()}}(Exception):
+class {{cookiecutter.library_name.replace('_', '').title()}}Exception(Exception):
     """base exception class for project"""
